f415cab8c4e83487ed42c4cf96688eeae9f3f563
kagose yusuke <>
2017-09-18 23:28:49 +0900

    add edit update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @portfolio_items = Portfolio.all
@@ -20,7 +22,25 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
+
+      def set_portfolio
+        @portfolio_item = Portfolio.find(params[:id])
+      end
       def portfolio_params
         params.require(:portfolio).permit(:title,:subtitle, :body)
       end
