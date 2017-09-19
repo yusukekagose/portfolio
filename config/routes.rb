05c750d715086fbded799259d90c9e23bce4bb16
kagose yusuke <>
2017-09-19 18:18:50 +0900

    customized login out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '',path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root 'pages#home'
   resources :portfolios,except: [:show]
   get 'angular-items', to: 'portfolios#angular'
