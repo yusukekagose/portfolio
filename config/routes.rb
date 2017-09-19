eefe6861bb39880bb98d2ca1c5b7ddf74496a473
kagose yusuke <>
2017-09-19 10:53:25 +0900

    fix portfolio path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  resources :portfolios
+  resources :portfolios,except: [:show]
+  get 'portfolio/:id', to:'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
