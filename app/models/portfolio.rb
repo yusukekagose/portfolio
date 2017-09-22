12fb8c512f534751128cbb354f5932178a38ede3
kagose yusuke <>
2017-09-22 13:20:40 +0900

    update portfolio mode add position column

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,12 +10,16 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'angular')
   end
 
-   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'ruby on rails') }
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'ruby on rails') }
 
-   after_initialize :set_defaults
+  after_initialize :set_defaults
 
-    def set_defaults
-      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-      self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
+
+  def self.by_position
+    order('position ASC')
+  end
 end
