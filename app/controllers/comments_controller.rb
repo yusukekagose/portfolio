4c8c998fd31e238a074800bde1724ccbb837e3c2
kagose yusuke <>
2017-09-24 14:57:25 +0900

    implementede action cable function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 		@comment = current_user.comments.build(comment_params)
 	end
 
-	def commnet_params
+	def comment_params
 		params.require(:comment).permit(:content)
 	end
 end
